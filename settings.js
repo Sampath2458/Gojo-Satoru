be940002ce9583a15323f97ffd7076e45e499d65
Sampath2458 <90492578+Sampath2458@users.noreply.github.com>
2022-07-07 19:21:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': '5c273af2-ad0b-411a-bde9-fff2d19b7f8a', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
